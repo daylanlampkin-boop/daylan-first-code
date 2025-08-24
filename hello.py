f5dbfba0bfada602597051e4da6825fdac9d70fa
daylanlampkin-boop <daylanlampkin@gmail.com>
2025-08-24 12:07:31 -0400

    Add files via upload

--- hello.py
+++ hello.py
@@ -2,3 +2,12 @@ print("Hello, Daylan! 🚀 You just ran your first program.")
 print("Daylan is learning to code!")
 print(5+3)
 print("in 5 years, I will be", 26 + 5, "years old")
+print("I am learning Python!")
+name= "Daylan"
+age= 26
+hobby= "learning to code"
+goal= "become a software developer"
+print("My name is", name)
+print("I am", age, "years old")
+print("My hobby is", hobby)
+print("My goal is to", goal)
